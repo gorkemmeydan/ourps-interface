05f164a75c32f60db7af85d16c18f4bb1a3bcdc5
Gorkem Meydan <gorkem.meydan@hotmail.com>
2021-09-23 17:02:15 +0300

    Add web3 metamask connection to connect wallet button

--- src/components/header/connect-wallet-button/connect-wallet-button.component.tsx
+++ src/components/header/connect-wallet-button/connect-wallet-button.component.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
+import { useEthers } from '@usedapp/core';
+
 import { ReactComponent as MetaMaskLogo } from '../../../assets/metamask-fox.svg';
 
 import './connect-wallet-button.styles.scss';
 
-const ConnectWalletButton: React.FC = () => (
-  <button type='button' className='connect-wallet-button'>
-    <div className='button-text'>Connect with MetaMask</div>
-    <MetaMaskLogo className='logo' />
-  </button>
-);
+const ConnectWalletButton: React.FC = () => {
+  const { activateBrowserWallet } = useEthers();
+
+  const handleConnectWallet = () => {
+    activateBrowserWallet();
+  };
+
+  return (
+    <button
+      type='button'
+      className='connect-wallet-button'
+      onClick={handleConnectWallet}
+    >
+      <div className='button-text'>Connect with MetaMask</div>
+      <MetaMaskLogo className='logo' />
+    </button>
+  );
+};
 
 export default ConnectWalletButton;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { DAppProvider } from '@usedapp/core';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={{}}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
