3dbcbec34d94ad7a859bb7eb7759d31988296107
Gorkem Meydan <gorkem.meydan@hotmail.com>
2021-09-23 16:40:19 +0300

    Add app header scaffold

---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './header.styles.scss';
+
+const Header: React.FC = () => (
+  <div className='header'>
+    <div className='app-name'>OuRPS - Decentralized Rock, Paper, Scissors!</div>
+  </div>
+);
+
+export default Header;
--- src/pages/homepage/homepage.component.tsx
+++ src/pages/homepage/homepage.component.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Header from '../../components/header/header.component';
+
 import './homepage.styles.scss';
 
-const HomePage: React.FC = () => <div className='homepage'>HomePage</div>;
+const HomePage: React.FC = () => (
+  <div className='homepage'>
+    <div className='header-container'>
+      <Header />
+    </div>
+  </div>
+);
 
 export default HomePage;
