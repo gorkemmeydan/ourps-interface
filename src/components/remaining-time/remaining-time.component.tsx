4ccc5d3d51dcd56f8a28164c6c55bb4a67bd8f26
Gorkem Meydan <gorkem.meydan@hotmail.com>
2021-09-25 23:35:50 +0300

    Add round end calculations to remaining time component

--- src/components/remaining-time/remaining-time.component.tsx
+++ src/components/remaining-time/remaining-time.component.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import useRoundEndTimestamp from '../../hooks/useRoundEndTimestamp';
+
 import './remaining-time.styles.scss';
 
 const RemainingTime: React.FC = () => {
-  const dummyRemainingTime = '03:21';
+  const time = useRoundEndTimestamp();
 
-  return <div className='remaining-time'>{dummyRemainingTime}</div>;
+  let convertedTime = '';
+  if (time !== undefined) {
+    const currentTime = new Date().valueOf() / 1000;
+
+    if (currentTime > time) {
+      convertedTime = 'round has been ended';
+    } else {
+      const diffTime = time - currentTime;
+      convertedTime = new Date(diffTime * 1000).toLocaleString();
+    }
+  }
+
+  return <div className='remaining-time'>{convertedTime}</div>;
 };
 
 export default RemainingTime;
